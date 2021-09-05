464f84eabe26a40d9803d137ea94368451d1127b
Humoyun <87017227+HumoyunDeveloper@users.noreply.github.com>
2021-09-05 09:21:00 +0500

    Converted to ES5

--- dist/consolejs.js
+++ dist/consolejs.js
@@ -4,101 +4,130 @@
     Description: Style your console!
 */
 
-(function (_o) {
-    "use strict";
-    const isSupported = typeof _o !== "undefined";
-    const isArray = function (_param) {
-        return /array/gi.test(_param.constructor.toString());
-    };
-    const checkAll = function (_param, _param2) {
-        return isArray(_param) ? _param.every(function (_val) {
-            return typeof _val === _param2;
-        }) : typeof _param === _param2 ? true : false;
-    };
-    const isObject = function (_param) {
-        return checkAll(_param, "object");
-    };
-    const isString = function (_param) {
-        return checkAll(_param, "string");
-    };
-    const isNumber = function (_param) {
-        return checkAll(_param, "number");
-    };
-    const doOpt = function () {
-        const cjs = {
-            log: function (_text) {
-                const props = this.getProps(_text);
-                if (props) {
-                    console["log"](props.g_text, ...props.styles);
-                } else {
-                    console['error']("Syntax hatolik \nMisol: cjs.log(\'<1>Salom dunyo</1>\')");
-                }
-            },
-            error: function (_text) {
-                const props = this.getProps(_text);
-                if (props) {
-                    console["error"](props.g_text, ...props.styles);
-                } else {
-                    console['error']("Syntax hatolik \nMisol: cjs.error(\'<1>Salom dunyo</1>\')");
-                }
+"use strict";
 
-            },
-            info: function (_text) {
-                const props = this.getProps(_text);
-                if (props) {
-                    console["info"](props.g_text, ...props.styles);
-                } else {
-                    console['error']("Syntax hatolik \nMisol: cjs.info(\'<1>Salom dunyo</1>\')");
-                }
-            },
-            warn: function (_text) {
-                const props = this.getProps(_text);
-                if (props) {
-                    console["warn"](props.g_text, ...props.styles);
-                } else {
-                    console['error']("Syntax hatolik \nMisol: cjs.warn(\'<1>Salom dunyo</1>\')");
-                }
-            },
-            getProps: function (_text) {
-                const regexp = /<(\d+)\s*(style=([\"\'])(.*?)\3)?>(.*?)<\/\1>/gi;
-                var props = {
-                    texts: [],
-                    sizes: [],
-                    styles: [],
-                    g_text: ""
-                };
-                if (regexp.test(_text)) {
-                    const infos = _text.match(regexp);
-                    for (let index = 0; index < infos.length; index++) {
-                        const innerRegx = /<(\d+)\s*(style=([\"\'])(.*?)\3)?>(.*?)<\/\1>/gi;
-                        const element = infos[index];
-                        const s_props = innerRegx.exec(element);
-                        const size = s_props[1];
-                        const text = s_props[5];
-                        const style = s_props[4];
-                        props.sizes.push(size);
-                        props.texts.push(text);
-                        props.styles.push(style ? "font-size:" + size + "px;" + style : "font-size:" + size + "px;");
-                    }
-                    for (let i = 0; i < props.texts.length; i++) {
-                        const text = props.texts[i];
-                        props.g_text += "%c" + text;
-                    }
-                } else {
-                    props = null;
-                }
-                return props;
-            }
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function(obj) { return typeof obj; } : function(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+(function(_o) {
+  "use strict";
+
+  var isSupported = typeof _o !== "undefined";
+  var isArray = function isArray(_param) {
+    return (/array/gi.test(_param.constructor.toString()));
+  };
+  var checkAll = function checkAll(_param, _param2) {
+    return isArray(_param) ? _param.every(function(_val) {
+      return (typeof _val === "undefined" ? "undefined" : _typeof(_val)) === _param2;
+    }) : (typeof _param === "undefined" ? "undefined" : _typeof(_param)) === _param2 ? true : false;
+  };
+  var isObject = function isObject(_param) {
+    return checkAll(_param, "object");
+  };
+  var isString = function isString(_param) {
+    return checkAll(_param, "string");
+  };
+  var isNumber = function isNumber(_param) {
+    return checkAll(_param, "number");
+  };
+  var doOpt = function doOpt() {
+    var cjs = {
+      /**
+       * @param string _text - Misol: <20 style='font-weight: bold'>Salom</20>
+       * @desc - cjs Elementlaridan (<SON>TEXT</SON>) foydalangan holda consolga habar jo'natadi. Agar "log" funksiyasi ichida cjs Elementi bo'lmasa "Syntax Xato" lik e'lon qilinadi.
+       **/
+      log: function log(_text) {
+        var props = this.getProps(_text);
+        if (props) {
+          var _console;
+
+          (_console = console)["log"].apply(_console, [props.g_text].concat(_toConsumableArray(props.styles)));
+        } else {
+          console['error']("Syntax hatolik \nMisol: cjs.log(\'<1>Salom dunyo</1>\')");
+        }
+      },
+      /**
+       * @param string _text - Misol: <20 style='font-weight: bold'>Salom</20>
+       * @desc - cjs Elementlaridan (<SON>TEXT</SON>) foydalangan holda consolga habar jo'natadi. Agar "error" funksiyasi ichida cjs Elementi bo'lmasa "Syntax Xato" lik e'lon qilinadi.
+       **/
+      error: function error(_text) {
+        var props = this.getProps(_text);
+        if (props) {
+          var _console2;
+
+          (_console2 = console)["error"].apply(_console2, [props.g_text].concat(_toConsumableArray(props.styles)));
+        } else {
+          console['error']("Syntax hatolik \nMisol: cjs.error(\'<1>Salom dunyo</1>\')");
+        }
+      },
+      /**
+       * @param string _text - Misol: <20 style='font-weight: bold'>Salom</20>
+       * @desc - cjs Elementlaridan (<SON>TEXT</SON>) foydalangan holda consolga habar jo'natadi. Agar "info" funksiyasi ichida cjs Elementi bo'lmasa "Syntax Xato" lik e'lon qilinadi.
+       **/
+      info: function info(_text) {
+        var props = this.getProps(_text);
+        if (props) {
+          var _console3;
+
+          (_console3 = console)["info"].apply(_console3, [props.g_text].concat(_toConsumableArray(props.styles)));
+        } else {
+          console['error']("Syntax hatolik \nMisol: cjs.info(\'<1>Salom dunyo</1>\')");
+        }
+      },
+      /**
+       * @param string _text - Misol: <20 style='font-weight: bold'>Salom</20>
+       * @desc - cjs Elementlaridan (<SON>TEXT</SON>) foydalangan holda consolga habar jo'natadi. Agar "warn" funksiyasi ichida cjs Elementi bo'lmasa "Syntax Xato" lik e'lon qilinadi.
+       **/
+      warn: function warn(_text) {
+        var props = this.getProps(_text);
+        if (props) {
+          var _console4;
+
+          (_console4 = console)["warn"].apply(_console4, [props.g_text].concat(_toConsumableArray(props.styles)));
+        } else {
+          console['error']("Syntax hatolik \nMisol: cjs.warn(\'<1>Salom dunyo</1>\')");
+        }
+      },
+      getProps: function getProps(_text) {
+        var regexp = /<(\d+)\s*(style=([\"\'])(.*?)\3)?>(.*?)<\/\1>/gi;
+        var props = {
+          texts: [],
+          sizes: [],
+          styles: [],
+          g_text: ""
         };
-        window.cjs ? true : window.cjs = cjs;
+        if (regexp.test(_text)) {
+          var infos = _text.match(regexp);
+          for (var index = 0; index < infos.length; index++) {
+            var innerRegx = /<(\d+)\s*(style=([\"\'])(.*?)\3)?>(.*?)<\/\1>/gi;
+            var element = infos[index];
+            var s_props = innerRegx.exec(element);
+            var size = s_props[1];
+            var text = s_props[5];
+            var style = s_props[4];
+            props.sizes.push(size);
+            props.texts.push(text);
+            props.styles.push(style ? "font-size:" + size + "px;" + style : "font-size:" + size + "px;");
+          }
+          for (var i = 0; i < props.texts.length; i++) {
+            var _text2 = props.texts[i];
+            props.g_text += "%c" + _text2;
+          }
+        } else {
+          props = null;
+        }
+        return props;
+      }
     };
-    const toString = Object.prototype.toString;
-    if (isSupported) {
-        doOpt();
-    } else {
-        /*DN*/
-    }
-
+    window.cjs ? true : window.cjs = cjs;
+  };
+  var toString = Object.prototype.toString;
+  if (isSupported) {
+    doOpt();
+  } else {
+    /*DN*/
+  }
 })(console);
 
 /*
@@ -109,3 +138,4 @@
     4 - STYLE
     5 - TEXT
 */
+
